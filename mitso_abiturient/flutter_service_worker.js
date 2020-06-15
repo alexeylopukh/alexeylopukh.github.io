94796567f9205327fe8a983b888c6b56599c7da3
Alexey Lopukh <lopukh911@gmail.com>
2020-06-15 10:03:09 +0300

    nm

--- mitso_abiturient/flutter_service_worker.js
+++ mitso_abiturient/flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "e7e2c116c99b9249f6388b9524f015b1",
+"assets/assets/cert.pem": "9f4e15454920d97e15b2af81d7bb0b51",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "42eed290e0f9d0a3aff093233d11d40b",
+"assets/LICENSE": "abfe09b2e80f20237331832fb3386930",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "aa3ce7c5eb4d087d188c1eb7b9a03422",
 "/": "aa3ce7c5eb4d087d188c1eb7b9a03422",
-"main.dart.js": "dcd5a5910f1a4a6ec90cbf2b5d106e14",
+"main.dart.js": "255f3a4fe8a2756d61c009e899f540fe",
 "manifest.json": "4d331ba0e9dac94075a2c156c80fabfb"
 };
 
